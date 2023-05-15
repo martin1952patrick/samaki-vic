a6dae93cd37bcd210ff2f27b801f5c68e9b8caa0
Martin_Omollo <martinpati1952@gmail.com>
2023-05-15 19:35:51 +0300

    add models

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from base.models import BaseModel
+import uuid
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from base.emails import send_account_activation_email
 
 # Create your models here.
 
@@ -9,3 +13,16 @@ class Profile(BaseModel):
     is_email_verified = models.BooleanField(default=False)
     email_token = models.CharField(max_length=100, null=True, blank=True)
     Profile_image = models.ImageField(upload_to = 'profile')
+
+
+@receiver(post_save , sender = User)
+def send_email_token(sender , instance, created, **kwargs):
+    try:
+        if created:
+            email_token = str(uuid.uuid4())
+            Profile.objects.create(user = instance, email_token = email_token)
+            email = instance.email
+            send_account_activation_email(email, email_token)
+
+    except Exception as e:
+        print(e)            
